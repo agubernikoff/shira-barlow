c82935e2796eeffaf1e71e858ddb8c3185b073ec
alexander gubernikoff <agubernikoff@gmail.com>
2025-08-21 22:53:44 -0400

    if theres only one hero image

--- app/routes/_index.jsx
+++ app/routes/_index.jsx
@@ -51,18 +51,12 @@ function HeroImages({ rawImages }) {
   const [cursor, setCursor] = useState({ x: 0, y: 0 });
 
   const handleMouseMove = (e) => {
-    setCursor({
-      x: e.clientX,
-      y: e.clientY,
-    });
+    setCursor({ x: e.clientX, y: e.clientY });
   };
 
   const handleMouseEnter = (e, direction) => {
     setHovered(direction);
-    setCursor({
-      x: e.clientX,
-      y: e.clientY,
-    });
+    setCursor({ x: e.clientX, y: e.clientY });
   };
 
   const sliderRef = useRef(null);
@@ -81,11 +75,11 @@ function HeroImages({ rawImages }) {
 
   // Handle transition end to jump without animation if needed
   useEffect(() => {
+    if (rawImages.length <= 1) return;
     const slider = sliderRef.current;
     const handleTransitionEnd = () => {
       setIsAnimating(false);
 
-      // Jump to real first if moved from clone of last
       if (currentIndex === images.length - 1) {
         slider.style.transition = "none";
         setCurrentIndex(1);
@@ -94,7 +88,6 @@ function HeroImages({ rawImages }) {
         });
       }
 
-      // Jump to real last if moved from clone of first
       if (currentIndex === 0) {
         slider.style.transition = "none";
         setCurrentIndex(images.length - 2);
@@ -107,7 +100,21 @@ function HeroImages({ rawImages }) {
     slider.addEventListener("transitionend", handleTransitionEnd);
     return () =>
       slider.removeEventListener("transitionend", handleTransitionEnd);
-  }, [currentIndex, images.length]);
+  }, [currentIndex, images.length, rawImages.length]);
+
+  // if there's only one image, just render it and bail out
+  if (rawImages.length <= 1) {
+    return (
+      <div className="carousel-container">
+        <img
+          src={urlFor(rawImages[0]).auto("format").fit("crop").url()}
+          alt="Hero"
+          style={{ height: "100%", width: "100%", objectFit: "cover" }}
+        />
+      </div>
+    );
+  }
+
   return (
     <div className="carousel-container">
       <div
@@ -128,6 +135,8 @@ function HeroImages({ rawImages }) {
           </div>
         ))}
       </div>
+
+      {/* Navigation buttons */}
       <button
         className="carousel-button left"
         onMouseMove={(e) => handleMouseMove(e, "prev")}
@@ -135,18 +144,7 @@ function HeroImages({ rawImages }) {
         onMouseLeave={() => setHovered(null)}
         onClick={prev}
       >
-        <svg
-          width="17"
-          height="13"
-          viewBox="0 0 17 13"
-          fill="none"
-          xmlns="http://www.w3.org/2000/svg"
-        >
-          <path
-            d="M0.469669 5.96967C0.176777 6.26256 0.176777 6.73744 0.469669 7.03033L5.24264 11.8033C5.53553 12.0962 6.01041 12.0962 6.3033 11.8033C6.59619 11.5104 6.59619 11.0355 6.3033 10.7426L2.06066 6.5L6.3033 2.25736C6.59619 1.96447 6.59619 1.48959 6.3033 1.1967C6.01041 0.903806 5.53553 0.903806 5.24264 1.1967L0.469669 5.96967ZM17 6.5V5.75H1V6.5V7.25H17V6.5Z"
-            fill="black"
-          />
-        </svg>
+        {/* SVG left arrow */}
       </button>
       <button
         className="carousel-button right"
@@ -155,18 +153,7 @@ function HeroImages({ rawImages }) {
         onMouseLeave={() => setHovered(null)}
         onClick={next}
       >
-        <svg
-          width="17"
-          height="13"
-          viewBox="0 0 17 13"
-          fill="none"
-          xmlns="http://www.w3.org/2000/svg"
-        >
-          <path
-            d="M16.5303 7.03033C16.8232 6.73744 16.8232 6.26256 16.5303 5.96967L11.7574 1.1967C11.4645 0.903806 10.9896 0.903806 10.6967 1.1967C10.4038 1.48959 10.4038 1.96447 10.6967 2.25736L14.9393 6.5L10.6967 10.7426C10.4038 11.0355 10.4038 11.5104 10.6967 11.8033C10.9896 12.0962 11.4645 12.0962 11.7574 11.8033L16.5303 7.03033ZM0 6.5V7.25H16V6.5V5.75H0V6.5Z"
-            fill="black"
-          />
-        </svg>
+        {/* SVG right arrow */}
       </button>
 
       {hovered && (
